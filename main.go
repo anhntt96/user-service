8950f14925dc4e94a439a5915e7aac45a065ad57
Albert Nguyen <tuananh.nguyentien@grabtaxi.com>
2024-04-27 16:48:08 +0700

    Init

--- main.go
+++ main.go
@@ -1,140 +1,138 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
+	"net/http"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/gin-gonic/gin"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
-	"golang.org/x/crypto/bcrypt"
 )
 
-// export GO111MODULE=on;
 type User struct {
-	gorm.Model
-	Name     string
-	Email    string
-	Password string `json:"-"`
+	ID    string `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
 }
 
-type Secret struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-}
-
-var db *gorm.DB
-
-func getDynamoDBConnection() {
-	secretName := "dev/clientID"
-	region := "us-east-1"
-	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(region))
-	if err != nil {
-		panic("unable to load SDK config, " + err.Error())
-	}
-
-	client := secretsmanager.NewFromConfig(cfg)
-
-	input := &secretsmanager.GetSecretValueInput{
-		SecretId:     aws.String(secretName),
-		VersionStage: aws.String("AWSCURRENT"), // VersionStage defaults to AWSCURRENT if unspecified
-	}
-
-	result, err := client.GetSecretValue(context.TODO(), input)
-	if err != nil {
-		panic("failed to retrieve secret from AWS Secrets Manager")
-	}
-	var secret Secret
-	json.Unmarshal([]byte(*result.SecretString), &secret)
-	db, err = gorm.Open("mysql", fmt.Sprintf("%s:%s@/dbname?charset=utf8&parseTime=True&loc=Local", secret.Username, secret.Password))
-	if err != nil {
-		panic("failed to connect database")
-	}
-}
+var db = dynamodb.New(session.New(), aws.NewConfig().WithRegion("us-west-2"))
 
 func main() {
-
-	getDynamoDBConnection()
-
-	defer db.Close()
-	db.AutoMigrate(&User{})
-
 	r := gin.Default()
 
-	r.GET("/users", GetUsers)
-	r.POST("/users", CreateUser)
-	r.GET("/users/:id", GetUser)
-	r.PUT("/users/:id", UpdateUser)
-	r.DELETE("/users/:id", DeleteUser)
+	r.POST("/users", func(c *gin.Context) {
+		var user User
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		createUser(user)
+		c.JSON(http.StatusOK, gin.H{"data": user})
+	})
+
+	r.GET("/users/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		user, err := getUser(id)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"data": user})
+	})
+
+	r.PUT("/users/:id", func(c *gin.Context) {
+		var user User
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		err := updateUser(user)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"data": user})
+	})
+
+	r.DELETE("/users/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		err := deleteUser(id)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"data": "User deleted!"})
+	})
 
 	r.Run()
 }
 
-func GetUsers(c *gin.Context) {
-	var users []User
-	if err := db.Find(&users).Error; err != nil {
-		c.JSON(500, gin.H{"error": "Error fetching users", "details": err.Error()})
-		return
+// Create a user
+func createUser(user User) {
+	av, _ := dynamodbattribute.MarshalMap(user)
+	input := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String("Users"),
 	}
-	c.JSON(200, users)
+	_, _ = db.PutItem(input)
 }
 
-func CreateUser(c *gin.Context) {
-	var user User
-	if err := c.ShouldBindJSON(&user); err != nil {
-		c.JSON(400, gin.H{"error": "Bad request"})
-		return
+// Get a user
+func getUser(id string) (*User, error) {
+	result, err := db.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String("Users"),
+		Key: map[string]*dynamodb.AttributeValue{
+			"ID": {
+				S: aws.String(id),
+			},
+		},
+	})
+	if err != nil {
+		return nil, err
 	}
-	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
-	user.Password = string(hashedPassword)
-	if err := db.Create(&user).Error; err != nil {
-		c.JSON(500, gin.H{"error": "Error creating user", "details": err.Error()})
-		return
-	}
-	c.JSON(200, user)
+
+	user := new(User)
+	err = dynamodbattribute.UnmarshalMap(result.Item, user)
+	return user, err
 }
 
-func GetUser(c *gin.Context) {
-	var user User
-	if err := db.First(&user, c.Param("id")).Error; err != nil {
-		c.JSON(404, gin.H{"error": "User not found", "details": err.Error()})
-		return
+// Update a user
+func updateUser(user User) error {
+	input := &dynamodb.UpdateItemInput{
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":n": {
+				S: aws.String(user.Name),
+			},
+			":e": {
+				S: aws.String(user.Email),
+			},
+		},
+		TableName: aws.String("Users"),
+		Key: map[string]*dynamodb.AttributeValue{
+			"ID": {
+				S: aws.String(user.ID),
+			},
+		},
+		ReturnValues:     aws.String("UPDATED_NEW"),
+		UpdateExpression: aws.String("set Name = :n, Email = :e"),
 	}
-	c.JSON(200, user)
+
+	_, err := db.UpdateItem(input)
+	return err
 }
 
-func UpdateUser(c *gin.Context) {
-	var user User
-	if err := db.First(&user, c.Param("id")).Error; err != nil {
-		c.JSON(404, gin.H{"error": "User not found", "details": err.Error()})
-		return
+// Delete a user
+func deleteUser(id string) error {
+	input := &dynamodb.DeleteItemInput{
+		TableName: aws.String("Users"),
+		Key: map[string]*dynamodb.AttributeValue{
+			"ID": {
+				S: aws.String(id),
+			},
+		},
 	}
-	if err := c.ShouldBindJSON(&user); err != nil {
-		c.JSON(400, gin.H{"error": "Bad request"})
-		return
-	}
-	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
-	user.Password = string(hashedPassword)
-	if err := db.Save(&user).Error; err != nil {
-		c.JSON(500, gin.H{"error": "Error updating user", "details": err.Error()})
-		return
-	}
-	c.JSON(200, user)
-}
 
-func DeleteUser(c *gin.Context) {
-	var user User
-	if err := db.First(&user, c.Param("id")).Error; err != nil {
-		c.JSON(404, gin.H{"error": "User not found", "details": err.Error()})
-		return
-	}
-	if err := db.Delete(&user).Error; err != nil {
-		c.JSON(500, gin.H{"error": "Error deleting user", "details": err.Error()})
-		return
-	}
-	c.JSON(200, gin.H{"success": "User deleted"})
+	_, err := db.DeleteItem(input)
+	return err
 }
